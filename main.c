68446d00240f217a12ef7b50bd3d39dd2a06b756
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-30 08:58:19 -0800

    Update _printf.c with custom conversion specifier for binary (%b)

--- main.c
+++ main.c
@@ -1,19 +1,6 @@
 #include "main.h"
 #include <limits.h>
 
-/*int main(void)
-{*/
-  /*  _printf("Hello, World!\n");
-    return 0;*/
-
-    /*_printf("This is a test: %d, %i\n", 42, -123);
-    _printf("Another test: %s\n", "Hello, World!");
-     return 0;*/
-	/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
     _printf("%b\n", 98);
